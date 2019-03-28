1324d90e711f2765b162b2a52698ae9d3b12f1e3
ZhonghengYang_minemine <mikeyyyang@outlook.com>
2019-03-28 14:16:41 -0400

    add path depend

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.static('./dist'));
+// app.get('/*', function (req, res) {
+//  res.sendFile(__dirname + '/dist/client-view-angular/index.html');
+// });
+// app.listen(process.env.PORT || 4100);
+
+
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(express.static('./dist'));
-app.get('/*', function (req, res) {
- res.sendFile(__dirname + '/dist/client-view-angular/index.html');
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/your_app_name'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/client-view-angular/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4100);
